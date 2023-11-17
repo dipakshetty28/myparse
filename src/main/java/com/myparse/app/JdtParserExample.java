f94290038d4b388f3fa0c899f7ecc0ef6aa5d204
Dipak Shetty <dipakshetty28@gmail.com>
2023-11-16 22:51:17 -0500

    missingJavadocs Coomit

--- src/main/java/com/myparse/app/JdtParserExample.java
+++ src/main/java/com/myparse/app/JdtParserExample.java
@@ -1,11 +1,12 @@
 package com.myparse.app;
 
 import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.compiler.IProblem;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.json.JSONObject;
 
@@ -21,7 +22,8 @@ public class JdtParserExample {
 	private static List<String> classNames = new ArrayList<String>();
 	private static List<String> variableNames = new ArrayList<String>();
 	private static List<String> methodNames = new ArrayList<String>();
-	private static List<String> unusedImports = new ArrayList<String>();
+	private static List<String> missingJavadocsClass = new ArrayList<String>();
+	private static List<String> missingJavadocsMethod = new ArrayList<String>();
 
 	public static void main(String[] args) {
 //		if (args.length != 1) {
@@ -36,7 +38,8 @@ public class JdtParserExample {
 		result.put("nonMatchingClasses", classNames);
 		result.put("nonMatchingVariables", variableNames);
 		result.put("nonMatchingFunctions", methodNames);
-		result.put("unusedImports", unusedImports);
+		result.put("missingJavadocsClass", missingJavadocsClass);
+		result.put("missingJavadocsMethod", missingJavadocsMethod);
 		System.out.println(result.toString());
 	}
 
@@ -55,23 +58,26 @@ public class JdtParserExample {
 	}
 
 	public static void processJavaFile(File file) throws IOException {
-		String sourceCode = new String(Files.readAllBytes(file.toPath()));
+		final String sourceCode = new String(Files.readAllBytes(file.toPath()));
 
-		ASTParser parser = ASTParser.newParser(AST.JLS15); // Adjust as needed
+		ASTParser parser = ASTParser.newParser(AST.JLS_Latest);
 		parser.setKind(ASTParser.K_COMPILATION_UNIT);
 
 		Map<String, String> options = JavaCore.getOptions();
-		JavaCore.setComplianceOptions(JavaCore.VERSION_15, options); // Adjust as needed
+		JavaCore.setComplianceOptions(JavaCore.VERSION_15, options);
 		parser.setCompilerOptions(options);
 
 		parser.setSource(sourceCode.toCharArray());
 
 		CompilationUnit cu = (CompilationUnit) parser.createAST(null);
 
-		cu.accept(new org.eclipse.jdt.core.dom.ASTVisitor() {
-			public boolean visit(org.eclipse.jdt.core.dom.TypeDeclaration node) {
+		cu.accept(new ASTVisitor() {
+			public boolean visit(TypeDeclaration node) {
 				String className = node.getName().getIdentifier();
 				classNames.add(className);
+				if (node.getJavadoc() == null && !missingJavadocsClass.contains(className)) {
+					missingJavadocsClass.add(className);
+				}
 				return true;
 			}
 
@@ -81,19 +87,16 @@ public class JdtParserExample {
 				return true;
 			}
 
-
 			public boolean visit(MethodDeclaration node) {
 				String methodName = node.getName().getIdentifier();
 				methodNames.add(methodName);
+				if (node.getJavadoc() == null && !missingJavadocsMethod.contains(methodName)) {
+					missingJavadocsMethod.add(methodName);
+				}
 				return true;
 			}
 
 		});
 
-		for (IProblem problem : cu.getProblems()) {
-			if (problem.getID() == IProblem.UnusedImport) {
-				unusedImports.add(problem.getMessage());
-			}
-		}
 	}
 }
